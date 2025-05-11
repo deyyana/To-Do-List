58e6c386cdcf856193751876d1c20121e0501a15
Dayana <dayanaramlan04@gmail.com>
2025-05-11 12:51:17 +0800

    phase 1 complete
    create header comp and task component

---
+++ Task_Tracker/src/component/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header className="bg-[#D9D9D9]">
+            <h1 className='text-center text-3xl'> Task Tracker</h1>
+            <p className='text-center'>Keep track of your tasks</p>
+        </header>
+    ); 
+};
+
+export default Header;
---
+++ Task_Tracker/src/component/Task.jsx
@@ -0,0 +1,15 @@
+
+function Task() {
+    return (
+        <div className="flex items-center mx-auto justify-between bg-[#D9D9D9] w-[70%] m-[5%] rounded-xl p-[10px] pl-[15px] shadow-sm">
+            <button className="inline-block">
+                <p>IS study chap 6,7,8</p>
+            </button>
+            <button className=" flex items-center justify-center bg-[#F5F5F5] rounded-full w-[30px] h-[30px] ">
+                <p >A</p>
+            </button>
+        </div>
+    );
+}
+
+export default Task;
